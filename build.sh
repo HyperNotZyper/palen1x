2f8afdc897b2c01935619034d545e0ee004fcbda
Hyper <69536374+HyperNotZyper@users.noreply.github.com>
2023-04-14 12:51:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -85,11 +85,10 @@ cat << ! | chroot rootfs /usr/bin/env PATH=/usr/bin:/usr/local/bin:/bin:/usr/sbi
 apk update
 apk upgrade
 apk add bash alpine-base usbmuxd ncurses udev openssh-client sshpass newt
+apk add gnome --update-cache --repository https://dl-cdn.alpinelinux.org/alpine/edge/community/ --allow-untrusted
+apk add linux-pam --repository https://dl-cdn.alpinelinux.org/alpine/edge/main/ --allow-untrusted
 apk add --no-scripts linux-lts linux-firmware-none
 apk add openbox arandr dbus sddm sddm-openrc
-apk add gnome --update-cache --repository https://dl-cdn.alpinelinux.org/alpine/edge/community/ --allow-untrusted
-apk add linux-pam --update-cache --repository https://dl-cdn.alpinelinux.org/alpine/edge/main/ --allow-untrusted
-apk add gnome-apps-extra
 apk add pipewire wireplumber gst-plugin-pipewire
 apk add iwd
 apk add elogind polkit-elogind
