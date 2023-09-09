db42a145b72d636fc5aecb42c769a4bda4216447
Hyper <69536374+HyperNotZyper@users.noreply.github.com>
2023-09-09 12:23:48 +0200

    revert :(

--- nightlybuild.sh
+++ nightlybuild.sh
@@ -116,7 +116,7 @@ mount -vt sysfs sysfs rootfs/sys
 mount -vt proc proc rootfs/proc
 cp /etc/resolv.conf rootfs/etc
 cat << ! > rootfs/etc/apk/repositories
-http://dl-cdn.alpinelinux.org/alpine/v3.13/main
+http://dl-cdn.alpinelinux.org/alpine/v3.12/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 !
